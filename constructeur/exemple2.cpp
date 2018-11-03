c82c2a7789a9e2155f2f62a9c16502d058710d25
sikomo <komodu59@gmail.com>
2018-11-03 20:10:22 +0100

    Creation du fichier exemple 2.

---
+++ constructeur/exemple2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Point
+{ int x, y;
+public:
+  Point(int abs, int ord)
+  {
+          x = abs; y = ord;
+          cout << " ++ Constructeur d'un point : " << x << " " << y << "\n";
+  }
+  ~Point()
+  {
+          cout << " ++ Destructeur d'un point : " << x << " " << y << "\n";
+  }
+};
+
+void fct(Point *);
+
+int main()
+{
+        Point * adr;
+        cout << "****** Debut main ******\n";
+        adr = new Point(3,7);
+        fct(adr);
+        cout << "****** Fin main ******\n";
+}
+
+void fct(Point * adp){
+  cout << "****** Debut fct ******\n";
+  delete adp;
+  cout << "****** Fin fct ******\n";
+}
