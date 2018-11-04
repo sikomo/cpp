d82275a2cb1f3e277b7d1d15b9d96835661bc9fe
sikomo <komodu59@gmail.com>
2018-11-04 22:33:20 +0100

    Correction du fichier pointcou.cpp.

--- CodeSource_ProgrammerEnC++9e/chap20/pointcou.cpp
+++ CodeSource_ProgrammerEnC++9e/chap20/pointcou.cpp
@@ -1,9 +1,9 @@
-##include <iostream>
+#include <iostream>
 using namespace std ;
 class point
 {  int x, y ;
   public :
-   point (int abs, int ord) 
+   point (int abs, int ord)
      { cout << "++ Constr. point \n" ; x=abs ; y=ord ;
      }
    ~point () { cout << "-- Destr. point \n" ; }
@@ -15,7 +15,7 @@ class point
 class coul
 {  short couleur ;
   public :
-   coul (int cl) 
+   coul (int cl)
      { cout << "++ Constr. coul \n" ; couleur = cl ;
      }
    ~coul () { cout << "-- Destr.  coul \n" ; }
@@ -24,13 +24,13 @@ class coul
      }
 } ;
 
-class pointcoul : public point, public coul 
+class pointcoul : public point, public coul
 {  public :
     pointcoul (int, int, int) ;
     ~pointcoul () { cout << "---- Destr. pointcoul \n" ; }
     void affiche () const
-     { point::affiche () ; coul::affiche () ; 
-     } 
+     { point::affiche () ; coul::affiche () ;
+     }
 } ;
 pointcoul::pointcoul (int abs, int ord, int cl) : point (abs, ord), coul (cl)
 {  cout << "++++ Constr. pointcoul \n" ;
@@ -41,8 +41,8 @@ int main()
    cout << "------------\n" ;
    p.affiche () ;                 // appel de affiche de pointcoul
    cout << "------------\n" ;
-   p.point::affiche () ;          // on force l’appel de affiche de point
+   p.point::affiche () ;          // on force lï¿½appel de affiche de point
    cout << "------------\n" ;
-   p.coul::affiche () ;           // on force l’appel de affiche de coul
+   p.coul::affiche () ;           // on force lï¿½appel de affiche de coul
    cout << "------------\n" ;
 }
