ff3d4219235a6d36bfc8020728bb4c2e04bc94ae
komo <sikomo59@gmail.com>
2019-02-09 03:11:47 +0100

    First commit of lecture.

--- tests/test5.cpp
+++ tests/test5.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
   cout << "Salut komo" << endl;
-  cout << "Salam Dounia !" << endl;
+  cout << "Salam Redounia !" << endl;
   return 0;
 }
